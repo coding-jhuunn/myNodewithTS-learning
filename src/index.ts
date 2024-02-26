8d9630998386ea99be68a9f3f847cda41e040c01
coding-jhuunn <codingatjhunn@gmail.com>
2024-02-26 11:38:10 +0800

    setup env for ts express js

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log("hello");
+
+// imnport express = from 'express';
+// import http from 'http';
+// import bodyParser from 'body-parser';
+// import CookieParser from 'cookie-parser';
+// import compression from 'compression';
+// import cors from 'cors';
