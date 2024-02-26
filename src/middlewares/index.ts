ab1313ec1142a9c0ac39ad296f53297e17c580b4
coding-jhuunn <codingatjhunn@gmail.com>
2024-02-26 21:12:32 +0800

    delete , update user and isOwner n isAuthenticated

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,6 +4,28 @@ import { get, merge } from "lodash";
 
 import { getUsersbySessionToken } from "../db/user";
 
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const curentUserId = get(req, "identity._id") as string;
+
+    if (!curentUserId) {
+      return res.sendStatus(403);
+    }
+    if (curentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
@@ -12,7 +34,7 @@ export const isAuthenticated = async (
   try {
     const sessionToken = req.cookies["myDATABASE-AUTH"];
     if (!sessionToken) {
-      return res.status(403);
+      return res.sendStatus(403);
     }
     const exisitingUser = await getUsersbySessionToken(sessionToken);
     if (!exisitingUser) {
